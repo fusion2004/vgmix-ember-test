ef4107394778fc3cd5b24a3beb1087f8e9f29de0
fusion2004 <fusion2004@gmail.com>
2013-07-04 12:54:40 -0400

    added ember dev/prod config to environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ VgmixEmberTest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.ember.variant = :production
 end
