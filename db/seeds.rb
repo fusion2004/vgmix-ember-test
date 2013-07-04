b084ae2b11cc57df71587d11362971ee5e0e0472
fusion2004 <fusion2004@gmail.com>
2013-07-04 13:24:04 -0400

    migrated and seeds written

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create [
+	{ name: 'fusion2004' },
+	{ name: 'sgtrama' }
+]
+
+a = User.find(1)
+b = User.find(2)
+
+Song.create [
+	{ name: 'ballsplosion', url: 'http://mp3.thasauce.net/rts/WillRock_MegaManMegaMan2-TimeStopper_RTS.mp3', user: a },
+	{ name: 'testy2', url: 'http://mp3.thasauce.net/rts/Gario_SonicAndKucklesSonic3DBlast-SkyPuppetry_RTS.mp3', user: b }
+]
