feffb56a8ec7f48f2de5891f73b8b139e3b588e4
fusion2004 <fusion2004@gmail.com>
2013-07-04 10:29:11 -0400

    static route for ember

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,6 @@
 class StaticController < ApplicationController
+
+  def index
+    render text: 'test'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 VgmixEmberTest::Application.routes.draw do
+
+  root to: 'static#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
